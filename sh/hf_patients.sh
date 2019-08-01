72c87c11b1041c85dbef7a0c2cd4a0a2ea4809a7
Jeremy Yang <jeremyjyang@gmail.com>
2019-08-01 16:09:41 -0600

    Ok.

--- sh/hf_patients.sh
+++ sh/hf_patients.sh
@@ -9,6 +9,8 @@ if [ "${TUNNELPORT}" ]; then
 	printf "TUNNELPORT = %s (SSH tunnel mode)\n" "${TUNNELPORT}"
 fi
 #
+dbargs="-dbname $DBNAME"
+#
 if [ "${TUNNELPORT}" ]; then
 	ssh -T -O "check" $DBHOST
 	rval="$?"
@@ -16,12 +18,15 @@ if [ "${TUNNELPORT}" ]; then
 	if [ "$rval" -ne 0 ]; then
 		ssh -f -N -T -M -4 -L ${TUNNELPORT}:localhost:${DBPORT} $DBHOST
 	fi
+	dbargs="$dbargs -dbport ${TUNNELPORT}"
+else
+	dbargs="$dbargs -dbport ${DBPORT}"
 fi
 #
 mvn --projects unm_biocomp_cerner exec:java \
 	-Dexec.mainClass="edu.unm.health.biocomp.cerner.hf.hf_patients" \
 	-Dexec.cleanupDaemonThreads=false \
-	-Dexec.args="-dbname $DBNAME $*"
+	-Dexec.args="$dbargs $*"
 #
 #if [ "${TUNNELPORT}" ]; then
 #	ssh -T -O "exit" $DBHOST
