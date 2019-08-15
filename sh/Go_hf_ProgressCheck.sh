8db8ad720927b46e8bc85c5e7391d6f75ee2fa19
Jeremy J Yang <jeremyjyang@gmail.com>
2019-08-15 15:46:12 -0600

    Ok.

--- sh/Go_hf_ProgressCheck.sh
+++ sh/Go_hf_ProgressCheck.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 #
-HOST="hsc-ctschf.health.unm.edu"
+###
+# Define DBHOST
+. ~/.healthfactsrc
+#
 DATADIR="/home/jjyang/src/cerner-tools/data"
 #
 ftypes="diagnosis lab medication"
 #
-n_sk=$(ssh $HOST cat $DATADIR/hf_neph_cohort.sk |wc -l)
+n_sk=$(ssh $DBHOST cat $DATADIR/hf_neph_cohort.sk |wc -l)
 #
 for ftype in $ftypes ; do
-	n=$(ssh $HOST cat $DATADIR/hf_neph_cohort_f_${ftype}.tsv |sed -e 's/,.*$//' |sort -u |wc -l)
+	n=$(ssh $DBHOST cat $DATADIR/hf_neph_cohort_f_${ftype}.tsv |sed -e 's/,.*$//' |sort -u |wc -l)
 	printf "Nephrology patient PKs done (facttype: $ftype): %d / %d (%d%%)\n" \
 		"$n" \
 		"$n_sk" \
